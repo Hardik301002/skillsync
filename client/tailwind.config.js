28750d8ebc373c9019b7888249cb578b8b0f8f3a
Hardik301002 <hardikgupta769@gmail.com>
2026-02-20 23:49:00 +0530

    fix some css file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <--- This checks ALL folders in src
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
